9f25167fd7c0937380ec81d14246928fd1692b57
alex solodkyi <alexander@residenthome.com>
2021-09-15 00:38:05 +0300

    refactor BlockfrostClient

--- src/main/scala/io/blockfrost/sdk/BlockfrostClient.scala
+++ src/main/scala/io/blockfrost/sdk/BlockfrostClient.scala
@@ -7,16 +7,20 @@ import io.blockfrost.sdk.common.Network
 import sttp.client3.SttpBackend
 
 class BlockfrostClient[F[_], P](config: BlockfrostClientConfig[F, P]) extends ApiClient[F, P]
-  with HealthApiImpl[F, P]
-  with MetricsApiImpl[F, P]
   with AccountsApiImpl[F, P]
   with AddressesApiImpl[F, P]
   with AssetsApiImpl[F, P]
   with BlockApiImpl[F, P]
   with EpochsApiImpl[F, P]
+  with HealthApiImpl[F, P]
+  with IpfsApiImpl[F, P]
   with LedgerApiImpl[F, P]
   with MetadataApiImpl[F, P]
-  with NetworkApiImpl[F, P] {
+  with MetricsApiImpl[F, P]
+  with NetworkApiImpl[F, P]
+  with NutlinkApiImpl[F, P]
+  with PoolsApiImpl[F, P]
+  with TransactionsApiImpl[F, P] {
   override implicit val sttpBackend: SttpBackend[F, P] = config.sttpBackend
   override implicit val apiKey: ApiKey = config.apiKey
   override val host: String = config.host
@@ -26,10 +30,10 @@ object BlockfrostClient {
   def apply[F[_], P](sttpBackend: SttpBackend[F, P], network: Network): BlockfrostClient[F, P] =
     new BlockfrostClient(BlockfrostClientConfig(sttpBackend, getApiKeyFromEnvVariables, network.url))
 
-  private def getApiKeyFromEnvVariables: String = sys.env.getOrElse("BLOCKFROST_API_KEY", throw new RuntimeException("Api key not found in environment variables"))
-
   def apply[F[_], P](sttpBackend: SttpBackend[F, P]): BlockfrostClient[F, P] =
     new BlockfrostClient(BlockfrostClientConfig(sttpBackend, getApiKeyFromEnvVariables, getHostFromEnvVariables))
+
+  private def getApiKeyFromEnvVariables: String = sys.env.getOrElse("BLOCKFROST_API_KEY", throw new RuntimeException("Api key not found in environment variables"))
 
   private def getHostFromEnvVariables: String = sys.env.getOrElse("BLOCKFROST_HOST", throw new RuntimeException("Blockfrost host not found in environment variables"))
 
