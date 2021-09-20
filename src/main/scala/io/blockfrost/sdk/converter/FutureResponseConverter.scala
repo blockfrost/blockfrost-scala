b82e902a8efe6fecf3b71a9555ba2675775c6995
alex solodkyi <alexandrsolodkyi@gmail.com>
2021-09-21 01:20:48 +0300

    convert api errors to ApiException

--- src/main/scala/io/blockfrost/sdk/converter/FutureResponseConverter.scala
+++ src/main/scala/io/blockfrost/sdk/converter/FutureResponseConverter.scala
@@ -1,16 +1,25 @@
 package io.blockfrost.sdk.converter
 
-import sttp.client3.{Response, ResponseException}
+import org.json4s.jackson.Serialization
+import org.json4s.{Formats, Serialization}
+import sttp.client3.{HttpError, Response, ResponseException}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object FutureResponseConverter {
-  implicit class FutureResponseOps[A](response: Future[Response[Either[ResponseException[String, Exception], A]]])(implicit ec: ExecutionContext) {
+  implicit class FutureResponseOps[A](response: Future[Response[Either[ResponseException[String, Exception], A]]])(implicit ec: ExecutionContext, formats: Formats, serialization: Serialization) {
     def extract: Future[A] = response.flatMap {
       _.body match {
         case Right(body) => Future.successful(body)
         case Left(error) => Future.failed(error)
       }
+    } recoverWith {
+      case HttpError(body: String, _) =>
+        val error = Serialization.read[ApiError](body)
+        Future.failed(ApiException(error))
     }
   }
+
+  case class ApiError(status_code: Int, error: String, message: String)
+  case class ApiException(error: ApiError) extends RuntimeException(error.message)
 }
